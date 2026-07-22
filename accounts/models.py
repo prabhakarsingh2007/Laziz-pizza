4cd7aaa8952eaf872acf7e445d03e85bc95ebada
prabhakarsingh2007 <prabhakarkumarsingh578@gmail.com>
2026-07-22 22:28:00 +0530

    feat: add account management forms and initialize local database

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import UserForm
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserForm()
+    return render(request, 'register.html', {'form': form})
+
+
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserForm()
+    return render(request, 'login.html', {'form': form})
